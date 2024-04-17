db4079253e8c1b2ab4d48366f495303a049ef13e
= <=>
2024-04-17 06:49:44 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,6 @@
 -- creates a new user 'hbnb_dev' without 'hbnb_dev_pwd' as password
 -- The user has all previleges on the database 'hbnb_dev_db' only
 -- the user has SELECT previlege on the dataase 'perfomance_schema' only
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
