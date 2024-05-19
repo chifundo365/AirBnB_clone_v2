bc2cb4116b1d9a484b1cead4efa65ee49f382ddc
= <=>
2024-05-19 08:09:35 -0700

    compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Generates a .tgz from the contents of web_static folder
+execute: fab -f 1-pack_web_static.py
+"""
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    dt = datetime.today().strftime("%Y%m%d%H%M%S")
+    archive = "web_static_{}.tgz".format(dt)
+    command = "tar -cvzf versions/{} web_static".format(archive)
+    local("mkdir -p versions")
+
+    create = local(command)
+
+    if create is not None:
+        return archive
+    return None
