236613a000e7a1a577ea2c42443fb80773c9a693
= <=>
2024-05-29 03:49:16 -0700

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,9 +31,9 @@ def c(text):
     return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', defaults={'text': 'is_cool'}, strict_slashes=False)
-def display_python(text):
+def display_python(text='is cool'):
     """ Handles the /python and /python/<text> route """
     return 'python {}'.format(escape(text.replace('_', ' ')))
 
