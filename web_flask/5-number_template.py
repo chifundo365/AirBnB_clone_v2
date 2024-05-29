f2e718349b06201ea1ff15d37f0f5602ede964c4
= <=>
2024-05-29 05:24:19 -0700

    Nummber template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,6 +47,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
+    """ Renders an html document and passes n to it """
     return render_template('5-number.html', n=n)
 
 
