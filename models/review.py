d45696eb2f31c5111b25701bab3accb312d8ce7b
chifundo biziweck <chifundobiziweck@gmail.com>
2024-06-03 11:53:00 +0000

    DBStorage-Review

--- models/review.py
+++ models/review.py
@@ -9,7 +9,7 @@ class Review(BaseModel, Base):
     __tablename__ = "reviews"
 
     if storage_t == "db":
-        __tablename__ = "Reviews"
+        __tablename__ = "reviews"
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
