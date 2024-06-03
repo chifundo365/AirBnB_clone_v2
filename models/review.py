8f1ec1733d177b97a1ee757738420f49ec51cd06
= <=>
2024-06-03 05:03:07 +0200

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -3,14 +3,15 @@
 from models.base_model import BaseModel, Base, String, ForeignKey, Column
 from models import storage_t
 
-class Review(BaseModel):
+
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "Reviews"
 
     if storage_t == "db":
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id") nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id", nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     else:
         place_id = ""
         user_id = ""
