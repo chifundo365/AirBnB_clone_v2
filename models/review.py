9a70562fd3b8f89f45bc1e771ca33706bc2d58ba
= <=>
2024-06-03 05:05:26 +0200

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -9,6 +9,7 @@ class Review(BaseModel, Base):
     __tablename__ = "Reviews"
 
     if storage_t == "db":
+        __tablename__ = "Reviews"
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id") nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
