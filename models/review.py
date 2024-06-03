e6adfaec4c6d80a6d6711dc2b07ed9d9779d6836
= <=>
2024-06-03 05:07:11 +0200

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from models import storage_t
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "Reviews"
+    __tablename__ = "reviews"
 
     if storage_t == "db":
         __tablename__ = "Reviews"
